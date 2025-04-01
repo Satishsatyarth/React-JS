b8ae4acf5b6d51da21a67f34fec0ff0fdf5b6401
satishsatyarth <satishsatyarth2002@gmail.com>
2025-04-01 18:15:33 +0530

    Initial code

---
+++ BankApp/Nri/NriLogin.jsx
@@ -0,0 +1,9 @@
+
+
+export default function NriLogin(){
+    return(
+        <div>
+            <h2>Nri Login</h2>
+        </div>
+    )
+}
---
+++ BankApp/Nri/NriRegister.jsx
@@ -0,0 +1,9 @@
+
+
+export default function NriRegister(){
+    return(
+        <div>
+            <h2>Nri Register</h2>
+        </div>
+    )
+}
